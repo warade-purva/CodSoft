246a1cbd90cf6b9285358ec764cbf665fe9db04b
warade-purva <purva.warade23@pccoepune.org>
2024-07-14 01:24:16 +0530

    Add files via upload

---
+++ Task2/Student_Grade_calculator/GradeCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject out of 100 " + i + ": ");
+            int marks = scanner.nextInt();
+            if (marks > 100) {
+                System.out.println("Please enter valid marks. Program will now terminate.");
+                scanner.close();
+                System.exit(1);
+            }
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / (numSubjects * 100) * 100;
+
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("----------------------------");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
